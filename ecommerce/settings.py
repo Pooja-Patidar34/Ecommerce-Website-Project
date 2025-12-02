8ce4ab1dd00427cb88c38e5c5871ac64859c93c5
Pooja-Patidar34 <pooja.chsr85@gmail.com>
2025-12-02 13:32:19 +0530

    changing database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -102,16 +102,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'Ecommerce_Db',  
-            'USER': 'root', 
-            'PASSWORD': 'Pooja@123', 
-            'HOST': 'localhost',  
-            'PORT': '3306',      
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
+    
 
 
 # Password validation
